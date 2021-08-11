11154823ea1a29c12cb9af5046bb04ad6051475b
ver0 <veronika_scheller@web.de>
2021-08-11 17:30:05 +0200

    Impftermine: sortierung klappt-> fehlt nur noch sortierung ohne Dummy Ort

--- nodeexpressapi/public/clientjs/vaccinationClient.js
+++ nodeexpressapi/public/clientjs/vaccinationClient.js
@@ -98,6 +98,7 @@ async function getUserData(){
         profile.latitude = userData.latitude;            
         profile.longitude = userData.longitude;   
         setFilter();
+        user=true;
     }
 
 }
@@ -115,9 +116,10 @@ function setFilter(){
     if(profile.radius != null ){
         document.getElementById("radius").value = profile.radius + " km";
     }
+    
 }
 
-function prepareVaccinationData(result){
+async function prepareVaccinationData(result){
     realResult =[];
     for(var i=0; i<result.length; i++ ){
        for(var j =0; j < result[i].Vaccines.length; j++){
@@ -127,10 +129,12 @@ function prepareVaccinationData(result){
             }
         }   
     }
-    if(user){//filer durchgehen
-
+    realResult.sort(GetSortOrder("Zentrumsname"));
+    if(await user){//filer durchgehen
+        getfilter()
     }
     else{
+       
         fillTable(realResult);
     }
     console.log("die vorbereiteten Results");
@@ -170,7 +174,10 @@ function fillTable(result){
 function buildTD(result){
 
         var s= document.createElement("td");
-            s.textContent="-";
+            if(result.Distance == null){
+                s.textContent="-";
+            }
+            else s.textContent=Math.round(result.Distance)+" km"; 
         var a= document.createElement("td");
             a.textContent=result.Zentrumsname;
         var b= document.createElement("td");
@@ -283,22 +290,23 @@ async function getfilter(){
         z.removeChild(tr[1]);
     }
 
-
-//erstmal nur nach stadt sortieren
+    var gefiltertResult = [];
+//erstmal nur nach stadt sortieren und Distance
     if(document.getElementById("city").value == ""){
-        fillTable(realResult);
+        gefiltertResult=filterImpfstoff(realResult);
+        fillTable(gefiltertResult);
     }
     else{
         var city = document.getElementById("city").value;
-        var dist = document.getElementById("radius").value;
+        var distValue = document.getElementById("radius").value;
+        
         //console.log("button wurde gedruckt");
         //console.log(city);
-        var gefiltertResult = [];
+        var copyResult= JSON.parse(JSON.stringify(realResult));
+        
+        var lat1, lon1;
 
-        //Vorbereitung adresse von Suchanfrage:
-        var adresse={
-            "Ort":""
-        }
+        
         
         
         //unction getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
@@ -306,68 +314,167 @@ async function getfilter(){
         profile.city= "Stuttgart";
         profile.latitude = 48.7758;
         profile.longitude = 9.1829;
+
         if(city == profile.city){
-            // berechnung mit koordinaten von city
-            for(var i in realResult){
-                console.log(i);
-                var lon2=realResult[i].Geocode.coordinates[0];
-                var lat2=realResult[i].Geocode.coordinates[1];
-                console.log("long "+lon2+" lat "+lat2);
-                var dist= getDistanceFromLatLonInKm(profile.latitude,profile.longitude,lat2,lon2);
-                realResult[i].Distance = dist;
-                
-            }
-            console.log(realResult);
+            lat1=profile.latitude;
+            lon1=profile.longitude;
         }
+        else{// berechnung der lat und lon vom Standort 
+
+        }
+            
+            // berechnung mit koordinaten von city
+            for(var i in copyResult){
+                console.log(i);
+                var lon2=copyResult[i].Geocode.coordinates[0];
+                var lat2=copyResult[i].Geocode.coordinates[1];
+                console.log("long "+lon2+" lat "+lat2);
+
+                var query="/data/geocode/distance?lat1="+lat1+"&long1="+lon1+"&lat2="+lat2+"&long2="+lon2;
+                try{
+                    var d= await fetch(query);
+                    dist = await d.text();
+                    dist = JSON.parse(dist);
+                    
+                }catch(e){
+                    console.log("Server is not responing");
+                }
+                // wenn error kommt
+                if(dist.error != undefined){
+                    // var a = document.createElement("caption");
+                    // a.textContent="Aktuell können keine Impfangebote angezeigt werden";
+                    // a.classList.add('error');
+                    // var table =document.getElementsByTagName("table")[0];
+                    // table.insertBefore(a, table.firstChild);
+                    
+                }
+                else if (dist != undefined){
+                    
+                    copyResult[i].Distance = dist;
+
+                }
+ 
+            }
+            //realResult=sortJSON(realResult,"Distance",123);
+            copyResult.sort(GetSortOrder("Distance"));
+            console.log(copyResult);
+            for (var i in copyResult){
+                if(copyResult[i].Distance <= distValue ) gefiltertResult.push(copyResult[i]);
+            }
+            
+            gefiltertResult = filterImpfstoff(gefiltertResult);
+            
+
+            if (!gefiltertResult.length > 0){
+                var a = document.createElement("caption");
+                a.textContent="Im angegebenen Ort und Umgebung sind derzeit keine Impftermine vorhanden";
+                a.classList.add('error');
+                var table =document.getElementsByTagName("table")[0];
+                table.insertBefore(a, table.firstChild);  
+                }
+            else{
+
+                fillTable(gefiltertResult);
+            }
+        
 
         //rechne geocode von city aus (eingegebener Value)
 
-        //var cityCords= await geocode.calcGeocodeForORT(adress);
+        
 
-        for (let i in result) {
-            if(result[i].Ort == city){
-                gefiltertResult.push(result[i]);
-            }
-        }
-        if (!gefiltertResult.length > 0){
-            var a = document.createElement("caption");
-            a.textContent="Im angegebenen Ort sind derzeit keine Impftermine vorhanden";
-            a.classList.add('error');
-            var table =document.getElementsByTagName("table")[0];
-            table.insertBefore(a, table.firstChild);  
-        }
-        else{
-            fillTable(gefiltertResult);
-        }
+        // for (let i in result) {
+        //     if(result[i].Ort == city){
+        //         gefiltertResult.push(result[i]);
+        //     }
+        // }
+        // if (!gefiltertResult.length > 0){
+        //     var a = document.createElement("caption");
+        //     a.textContent="Im angegebenen Ort sind derzeit keine Impftermine vorhanden";
+        //     a.classList.add('error');
+        //     var table =document.getElementsByTagName("table")[0];
+        //     table.insertBefore(a, table.firstChild);  
+        // }
+        // else{
+        //     fillTable(gefiltertResult);
+        // }
     }
 
 
     // hier Impfpräferenz beachten
-    var biontech = document.getElementById("biontech").checked;
-    var moderna = document.getElementById("moderna").checked;
-    var astra = document.getElementById("astra").checked;
-    var johnson = document.getElementById("johnson").checked;
+    // var biontech = document.getElementById("biontech").checked;
+    // var moderna = document.getElementById("moderna").checked;
+    // var astra = document.getElementById("astra").checked;
+    // var johnson = document.getElementById("johnson").checked;
     //console.log(" checks: biontech: "+biontech+"\n moderna: "+moderna+"\n astra: "+astra+"\n johnson: "+johnson);
 
     
 }
-// weg machen nicht vergessen -- ist geklaut
-function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2 - lat1);  // deg2rad below
-    var dLon = deg2rad(lon2 - lon1);
-    var a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2)
-        ;
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c; // Distance in km
-    return d;
+function filterImpfstoff(gefiltertResult){
+    var biontech = document.getElementById("biontech").checked;
+    var moderna = document.getElementById("moderna").checked;
+    var astra = document.getElementById("astra").checked;
+    var johnson = document.getElementById("johnson").checked;
+    var newResults= [];
+    
+    if((biontech && moderna && astra && johnson)||(!biontech && !moderna && !astra && !johnson))
+    {
+        return gefiltertResult;
+    }
+    for(var j =0 ;j< gefiltertResult.length;j++ ){
+        label:
+        for(var i =0; i < gefiltertResult[j].Vaccines.length; i++){
+            console.log("Vaccineslänge: "+gefiltertResult[j].Vaccines.length);
+            var stoff= gefiltertResult[j].Vaccines[i].ID
+            console.log(stoff);
+            if(gefiltertResult[j].Vaccines[i].Available !=undefined && gefiltertResult[j].Vaccines[i].Available){
+                switch (stoff){
+                    case "biontech":
+                        if(biontech){
+                            newResults.push(gefiltertResult[j]);
+                            break label;
+                        };
+                        break;
+                    case "moderna":
+                        if(moderna){
+                            newResults.push(gefiltertResult[j]);
+                            break label;
+                        };
+                        break;
+                    case "astra_zeneca":
+                        if(astra){
+                            newResults.push(gefiltertResult[j]);
+                            break label;
+                        };
+                        break;
+                    case "johnsonjohnson":
+                        if(johnson){
+                            newResults.push(gefiltertResult[j]);
+                            break label;
+                        };
+                        break;
+                }
+            }
+            //console.log(i);
+            //console.log(stoff)
+            //console.log(impfstoffe);
+            
+        }
+    }
+    return newResults;
 }
-function deg2rad(deg) {
-    return deg * (Math.PI / 180)
-}
+
+
+function GetSortOrder(prop){
+    return function(a,b){
+       if( a[prop] > b[prop]){
+           return 1;
+       }else if( a[prop] < b[prop] ){
+           return -1;
+       }
+       return 0;
+    }
+ }
+
 
 
 
@@ -402,6 +509,6 @@ function setLoginStatus(data){
         document.getElementById("RefToLogin").textContent="Abmelden";
         document.getElementById("RefToLogin").setAttribute("href", "/logout");
         getUserData();
-        user=true;
+        
     }else user = false;
 }
