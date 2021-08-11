11743aa8b68c569e6d706d3978fbd1eafce98310
Ulrich Michael <ulrich1999@web.de>
2021-08-11 11:49:08 +0200

    added getDistance function & route for geocoding

--- nodeexpressapi/routes/data.js
+++ nodeexpressapi/routes/data.js
@@ -156,7 +156,33 @@ router.get('/news', async (req, res) => {
 	}
 })
 
+//Zugriff über /data/geocode/distance?lat1=X&long1=Y&lat2=C&long2=B
+router.get('/geocode/distance', (req,res) =>{
+	const lat1=req.query.lat1;
+	const lat2=req.query.lat2;
+	const lon1=req.query.long1;
+	const lon2=req.query.long2
+	if(lat1==undefined || lat2== undefined || lon1 == undefined|| lon2==undefined) res.send("Parameters not defined")
+	res.send(String(getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2)))
+})
 
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+		var R = 6371; // Radius of the earth in km
+		var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+		var dLon = deg2rad(lon2 - lon1);
+		var a =
+			Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+			Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+			Math.sin(dLon / 2) * Math.sin(dLon / 2)
+			;
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		var d = R * c; // Distance in km
+		return d;
+	}
+	
+	function deg2rad(deg) {
+		return deg * (Math.PI / 180)
+	}
 //Sends back {error:true,{no_data_from:X}} in case of error
 async function getDistrictsFormated() {
 	const dbData_collection = "districtsBW"
